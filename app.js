34afdc1c2cc1acbf5033ad077838415e3035ab12
Binod <binod@Binitas-MacBook-Air.local>
2018-12-13 20:22:17 -0700

    Commit 1- Verbal questions

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("JS works!!");
+
+//1. parameter is what is called when defining a function
+//   Argument is what you inpt after the function is invoked
+
+
+//2. a return function outputs the value, while console.log prints the values in the console.
