d318330a4a0f06464ccc219bb3eda89f25f864b4
Binod <binod@Binitas-MacBook-Air.local>
2018-12-14 09:02:43 -0700

    Commit 2 - Palindrome

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 console.log("JS works!!");
 
 //1. parameter is what is called when defining a function
-//   Argument is what you inpt after the function is invoked
+    //1.   Argument is what you inpt after the function is invoked
 
+    //2. a return function outputs the value, while console.log prints the values in the console.
 
-//2. a return function outputs the value, while console.log prints the values in the console.
+    //3. We can use the return value later down the road.
+
+//2. Palindrome Again
+
+function sumDigits() {
+    var value = document.getElementById("thenumber").value,
+        sum = 0;
+
+  while (value) {
+      sum += value % 10;
+      value = Math.floor(value / 10);
+  }
+  
+  var rezultat = document.getElementById("result");
+  rezultat.value = sum;
+}
+
