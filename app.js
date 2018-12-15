cb42d8808dba91bd971ca04d4ea6bceac5c70544
Binod <binod@Binitas-MacBook-Air.local>
2018-12-14 21:16:09 -0700

    Commit 4 - Pythagoras.

--- app.js
+++ app.js
@@ -8,17 +8,32 @@ console.log("JS works!!");
     //3. We can use the return value later down the road.
 
 //2. Palindrome Again
-
-function sumDigits() {
-    var value = document.getElementById("thenumber").value,
-        sum = 0;
-
-  while (value) {
-      sum += value % 10;
-      value = Math.floor(value / 10);
+function checkPalindrome(word){
+    const drow = [...word].reverse().join('');
+    return drow === word;
   }
+  console.log(checkPalindrome('radar'));
+  console.log(checkPalindrome('Borscht'));
+
+//3. 
+
+function sumDigits(n) {
+	var sum = 0;
+	var string = n.toString();
+ 	for(i=0; i < string.length; i++){
+		sum = sum + parseInt(string.substring(i, i+1));
+	}
+ 	return sum;
+} 
+console.log(sumDigits(176878));
   
-  var rezultat = document.getElementById("result");
-  rezultat.value = sum;
-}
+//4. Pythogoras
+  const calculateSide = (sideA, sideB) => {
+      return Math.hypot(sideA, sideB);
+  }
+
+  console.log(calculateSide(8, 6));
+  
+//5.
+  
 
