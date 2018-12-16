330a6fd5525b6cb9c4856322c997c8eb889a3645
Binod <binod@Binitas-MacBook-Air.local>
2018-12-15 18:56:18 -0700

    Homework 4 all done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("JS works!!");
 
-//1. parameter is what is called when defining a function
+//1. parameter is what is called when defining a function/what goes inside the function
     //1.   Argument is what you inpt after the function is invoked
 
     //2. a return function outputs the value, while console.log prints the values in the console.
@@ -28,12 +28,35 @@ function sumDigits(n) {
 console.log(sumDigits(176878));
   
 //4. Pythogoras
-  const calculateSide = (sideA, sideB) => {
+  const calculateSide = (sideA, sideB) => {8
       return Math.hypot(sideA, sideB);
   }
 
   console.log(calculateSide(8, 6));
   
 //5.
+function sumDigits() {
+    var firstNum = 0;
+    for (var i=0; i < arguments.length; i++) {
+        firstNum += arguments[i];
+    }
+    return firstNum;
+ }
+ 
+ console.log(sumDigits(46, 54));
+
+ //6.
+function checkPrime (num){
+    for(let i = 2; i <= Math.sqrt(num); i++){
+        if(num % i === 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
+
+console.log(checkPrime(12));
   
 
